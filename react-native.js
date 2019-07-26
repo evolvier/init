abc74ba2857dcfa22a7f6e5f07fd8a5d020e096b
Vishnu P S <vishnu@evolvier.com>
2019-07-27 01:25:01 +0530

    remove .git

--- react-native.js
+++ react-native.js
@@ -5,12 +5,11 @@ const [,, ...args] = process.argv
 var shell = require("shelljs");
 var _ = require('lodash');
 
-console.log(args[0])
 if(args[0] === "init" && args[1]) {
 	shell.exec("git clone https://gitlab.com/evolvier/react-native-starter.git '"+_.replace(args[1]," ","")+"'");
 	shell.exec("cd "+_.replace(args[1]," ","")+" && npm i");
 	args[2] = args[2]?args[2]:"com.evolvier."+_.camelCase(args[1]);
-	// console.log("cd "+args[1]+" && npm run rename '"+args[1]+"' "+ args[2])
 	shell.exec("cd "+_.replace(args[1]," ","")+" && npm run rename '"+args[1]+"' "+ args[2]);
+	shell.exec("cd "+_.replace(args[1]," ","")+" && rm -rf .git");
 }
 
